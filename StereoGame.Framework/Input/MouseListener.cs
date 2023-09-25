7b3f38973d3c43f8634b4d1579dcf3bc9f6a9cd7
Jean Pascal Bellot <jebeli2015@outlook.com>
2023-09-25 11:35:20 +0200

    clicks added

--- StereoGame.Framework/Input/MouseListener.cs
+++ StereoGame.Framework/Input/MouseListener.cs
@@ -11,7 +11,11 @@
         private TimeSpan currentTime;
         private MouseState currentState;
         private MouseState previousState;
-
+        private MouseEventArgs? mouseDownArgs;
+        private MouseEventArgs? previousClickArgs;
+        private int doubleClickMilliseconds = 500;
+        private int dragThreshold = 2;
+        private bool hasDoubleClicked;
 
         public event EventHandler<MouseEventArgs>? MouseDown;
         public event EventHandler<MouseEventArgs>? MouseUp;
@@ -55,6 +59,17 @@
                 var args = new MouseEventArgs(currentState, previousState, button, currentTime);
 
                 MouseDown?.Invoke(this, args);
+                mouseDownArgs = args;
+                if (previousClickArgs != null)
+                {
+                    var clickMilliseconds = (currentTime - previousClickArgs.Time).TotalMilliseconds;
+                    if (clickMilliseconds <= doubleClickMilliseconds)
+                    {
+                        MouseDoubleClicked?.Invoke(this, args);
+                        hasDoubleClicked = true;
+                    }
+                    previousClickArgs = null;
+                }
             }
         }
 
@@ -64,10 +79,30 @@
                 (getButtonState(previousState) == ButtonState.Pressed))
             {
                 var args = new MouseEventArgs(currentState, previousState, button, currentTime);
+                if (mouseDownArgs != null && mouseDownArgs.Button == args.Button)
+                {
+                    var clickMovement = Distance(args.X, args.Y, mouseDownArgs.X, mouseDownArgs.Y);
+                    if (clickMovement < dragThreshold)
+                    {
+                        if (!hasDoubleClicked)
+                        {
+                            MouseClicked?.Invoke(this, args);
+                        }
+                        else
+                        {
 
+                        }
+                    }
+                }
                 MouseUp?.Invoke(this, args);
+                hasDoubleClicked = false;
+                previousClickArgs = args;
             }
         }
 
+        private static int Distance(int x1, int y1, int x2, int y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
     }
 }
